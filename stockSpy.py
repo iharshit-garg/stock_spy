3a8491d678470b5ebd16b6fd0136f3ea08487738
Harshit Garg <harshitgarg@Harshits-MacBook-Air.local>
2025-10-27 13:49:36 -0400

    - Added menu to choose between lookup and stock search. - Added a for loop in lookup functin to only show symbol and the name of the instrument - Implemented info method in stock class to display information about a stock.

--- stockSpy.py
+++ stockSpy.py
@@ -1,26 +1,42 @@
 import yfinance as yf
 
 def main():
-    NAME = input("Enter stock name (Ex: Apple, Nvidia): ").lower()
-    TYPE = input("What's the type of the financial instrument: ").lower()
-    lookup(NAME,TYPE)
 
-def lookup(security_name, instrument_type):
+    print("Stock Spy Menu")
+    print("1. Lookup")
+    print("2. Get Stock Data")
+    choice = int(input())
+    NAME = input("Enter stock name (Ex: Apple, Nvidia): ").lower()
+    
+    if choice == 1:
+        lookup(NAME)
+
+    elif choice == 2:
+        stock(NAME)
+
+def lookup(security_name):
+    instrument_type = input("Enter instrument type: ").lower()
     dat = yf.Lookup(security_name)
 
     if instrument_type == 'cryptocurrency':
-        print(dat.cryptocurrency)
+        print(dat.cryptocurrency['shortName'])
+
     elif instrument_type == 'etf':
-        print(dat.etf)
+        print(dat.etf['shortName'])
+
     elif instrument_type == 'stock':
-        print(dat.stock)
+        for idx, row in dat.stock.iterrows():   #This loop iterates over dataframe to fetch symbols and the short name of the security. idx -> 'Symbol'
+             print(f"Symbol: {idx}, Short Name: {row['shortName']}")
+
     elif instrument_type == 'index':
-        print(dat.index)
+        print(dat.index['shortName'])
+
     else:
         print("Wrong Financial Instrument Type")
 
 def stock(stock_name):
-          pass
+    dat = yf.Ticker(stock_name)
+    print(dat.info)
           
 if __name__ == "__main__":
     main()
