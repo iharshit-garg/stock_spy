288c29486aca8e63950a45a7cd668def1a0c70aa
Harshit Garg <harshitgarg@Harshits-MacBook-Air.local>
2025-10-29 11:19:12 -0400

    created separate python file for lookup function

---
+++ lookup.py
@@ -0,0 +1,22 @@
+import yfinance as yf
+
+def lookup(security_name):
+    
+    instrument_type = input("Enter instrument type: ").lower()
+    dat = yf.Lookup(security_name)
+
+    if instrument_type == 'cryptocurrency':
+        print(dat.cryptocurrency['shortName'])
+
+    elif instrument_type == 'etf':
+        print(dat.etf['shortName'])
+
+    elif instrument_type == 'stock':
+        for idx, row in dat.stock.iterrows():   #This loop iterates over dataframe to fetch symbols and the short name of the security. idx -> 'Symbol'
+             print(f"Symbol: {idx}, Short Name: {row['shortName']}")
+
+    elif instrument_type == 'index':
+        print(dat.index['shortName'])
+
+    else:
+        print("Wrong Financial Instrument Type")
