1c2a9306cba6c75675a7454418d47c116e70b37c
Harshit Garg <harshitgarg@Harshits-MacBook-Air.local>
2025-10-29 13:17:42 -0400

    - Implemented functionality to automatically grab symbol and display info - User has to choose the security

--- lookup.py
+++ lookup.py
@@ -1,4 +1,5 @@
-import yfinance as yf   
+import yfinance as yf
+from ticker import *
 
 def lookup(security_name):
     
@@ -9,10 +10,26 @@ def lookup(security_name):
     if instrument_type in instrumentTypeFields:
         result = getattr(dat, instrument_type)
 
+        lookedSymbols = []
         i=1
         #This loop iterates over dataframe to fetch symbols and the short name of the security. idx -> 'Symbol'
         for idx, row in result.iterrows():
                 print(f"{i}. Symbol: {idx}, Short Name: {row['shortName']}", end="\n\n")
+                lookedSymbols.append(idx)
                 i+=1
+        
+        getSymbol = input(f"Select a number (1-{len(lookedSymbols)}): ")
+        try:
+            getSymbol = int(getSymbol)
+            if 1 <= getSymbol <= len(lookedSymbols):
+                chosen_symbol = lookedSymbols[getSymbol - 1]
+                print(f"You selected: {chosen_symbol}")
+                ticker(chosen_symbol)
+            else:
+                 print("Out of range!")
+
+        except ValueError:
+             print("Invalid!")
+
     else:
          print("Invalid type!")
