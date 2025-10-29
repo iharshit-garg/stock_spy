e99b1bed2e64bd91f37bf64812b4af99fd1f0571
Harshit Garg <harshitgarg@Harshits-MacBook-Air.local>
2025-10-29 12:38:01 -0400

    added option to exit

--- stockSpy.py
+++ stockSpy.py
@@ -1,14 +1,17 @@
 import yfinance as yf
 from lookup import *
 from ticker import *
+import sys
 
 def main():
-
+    print("==============")
     print("Stock Spy Menu")
-    print("1. Lookup")
-    print("2. Get Stock Data")
-    choice = int(input())
-    NAME = input("Enter stock name (Ex: Apple, Nvidia): ").lower()
+    print("==============")
+    print("1. Look")
+    print("2. Get Info")
+    print("0. Exit")
+    choice = int(input("\nSelect an option: "))
+    NAME = input("Enter stock name (Ex: Apple, Nvidia): ").lower() if choice != 0 else None
     
     if choice == 1:
         lookup(NAME)
@@ -16,5 +19,8 @@ def main():
     elif choice == 2:
         ticker(NAME)
           
+    else:
+        sys.exit("Goodbye!")
+
 if __name__ == "__main__":
     main()
