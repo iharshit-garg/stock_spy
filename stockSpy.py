a367964db36f2e41f68e5f463613892728403471
Harshit Garg <harshitgarg@Harshits-MacBook-Air.local>
2025-10-29 13:20:43 -0400

    - Removed menu - The program asks for the name and calls lookup function to get info

--- stockSpy.py
+++ stockSpy.py
@@ -1,26 +1,14 @@
 import yfinance as yf
 from lookup import *
 from ticker import *
-import sys
 
 def main():
     print("==============")
-    print("Stock Spy Menu")
+    print("Welcome to Stock Spy!")
     print("==============")
-    print("1. Look")
-    print("2. Get Info")
-    print("0. Exit")
-    choice = int(input("\nSelect an option: "))
-    NAME = input("Enter stock name (Ex: Apple, Nvidia): ").lower() if choice != 0 else None
-    
-    if choice == 1:
-        lookup(NAME)
 
-    elif choice == 2:
-        ticker(NAME)
-          
-    else:
-        sys.exit("Goodbye!")
+    NAME = input("Enter stock name (Ex: Apple, Nvidia): ").lower()
+    lookup(NAME)
 
 if __name__ == "__main__":
     main()
