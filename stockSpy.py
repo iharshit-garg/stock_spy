e7b291323f4e4a57509185f166b6c54f586f17c5
Harshit Garg <harshitgarg@Harshits-MacBook-Air.local>
2025-10-29 21:17:27 -0400

    added while loop and choice to exit when the user want so that user doesn't have to re-run the program

--- stockSpy.py
+++ stockSpy.py
@@ -1,14 +1,22 @@
 import yfinance as yf
 from lookup import *
 from ticker import *
+import time
+import sys
 
 def main():
-    print("=====================")
-    print("Welcome to Stock Spy!")
-    print("=====================")
+    while True:
+        print("=====================")
+        print("Welcome to Stock Spy!")
+        print("=====================")
 
-    NAME = input("Enter stock name (Ex: Apple, Nvidia): ").lower()
-    lookup(NAME)
+        NAME = input("Enter stock name (Ex: Apple, Nvidia): ").lower()
+        lookup(NAME)
+
+        time.sleep(5)
+        exit_user = input("Do you want to exit [yes/no]: ").lower()
+        if exit_user =='yes':
+            sys.exit("GoodBye!")
 
 if __name__ == "__main__":
     main()
