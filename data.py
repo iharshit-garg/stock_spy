33e7bfc26140f8520564d8ef3c005ca554ff1428
Harshit Garg <harshitgarg@Harshits-MacBook-Air.local>
2026-02-03 08:26:38 -0500

    didn't assigned the value while calling the function which resulted in an error

--- data.py
+++ data.py
@@ -2,4 +2,9 @@ import yfinance as yf
 
 def get_history(stock_name, period = None, interval = "1d", auto_adjust = True, group_by = 'column'):
     dat = yf.Tickers(stock_name)
-    return dat.history(period, interval, auto_adjust, group_by)
+    return dat.history(
+        period = period,
+        interval = interval,
+        auto_adjust = auto_adjust,
+        group_by = group_by
+        )
