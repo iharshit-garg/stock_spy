e7346c812c6c35e7b0888b3b2dbf579323bdcf99
Harshit Garg <harshitgarg@Harshits-MacBook-Air.local>
2026-02-05 08:08:08 -0500

    moved the empty dataframe check code to right place

--- data.py
+++ data.py
@@ -2,16 +2,16 @@ import yfinance as yf
 
 def get_history(stock_name, period = None, interval = "1d", auto_adjust = True, group_by = 'ticker'):
     dat = yf.Tickers(stock_name)
-    if dat is None:#checking if the dataframe is empty
+    result =  dat.history(
+        period = period,
+        interval = interval,
+        auto_adjust = auto_adjust,
+        group_by = group_by
+        )
+    if result is None or result.empty:#checking if the dataframe is empty
         return None
     else:
-        return dat.history(
-            period = period,
-            interval = interval,
-            auto_adjust = auto_adjust,
-            group_by = group_by
-            )
-
+        return result
 '''
 For single ticker, use yf.Ticker(stock_name).history(**args)
 '''
