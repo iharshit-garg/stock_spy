ec2da50f0a2eed70b4f60316dbb8d63289615f9e
Harshit Garg <harshitgarg@Harshits-MacBook-Air.local>
2026-02-04 22:13:53 -0500

    added comments

--- lookup.py
+++ lookup.py
@@ -1,8 +1,8 @@
 import yfinance as yf
 
-def lookup(security_name, instrument_type):
+def lookup(security_name: str, instrument_type: str):
     
-    instrumentTypeFields = ['cryptocurrency', 'etf', 'stock', 'index']
+    instrumentTypeFields = ['cryptocurrency', 'etf', 'stock', 'index'] #supported instrument types
     dat = yf.Lookup(security_name)
     
     if instrument_type in instrumentTypeFields:
@@ -11,17 +11,17 @@ def lookup(security_name, instrument_type):
 
         if df is None or df.empty: #checking if the dataframe is empty
              print("No results")
-             return None
+             return None #returning none so that cli can decide what to show to the user
         else:
-             i=1
-             for idx, name in df['shortName'].head(10).items():
+             i = 1
+             for idx, name in df['shortName'].head(10).items(): #getting shortName field from the dataframe, showing only top 10 items
                 print(i, idx, name)
                 lookedSymbols.append(idx)
-                i+=1
+                i += 1
         
         getSymbol = input(f"Select a number (1-{len(lookedSymbols)}): ")
         try:
-            getSymbol = int(getSymbol)
+            getSymbol = int(getSymbol) #type -> int
             if 1 <= getSymbol <= len(lookedSymbols):
                 chosen_symbol = lookedSymbols[getSymbol - 1]
                 print(f"You selected: {chosen_symbol}")
