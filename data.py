71312cede84652d94fbf2c656b867c2b0b8ed0aa
Harshit Garg <harshitgarg@Harshits-MacBook-Air.local>
2026-02-05 09:36:40 -0500

    disabled the progres bar

--- data.py
+++ data.py
@@ -1,12 +1,13 @@
 import yfinance as yf
 
-def get_history(stock_name, period = None, interval = "1d", auto_adjust = True, group_by = 'ticker'):
+def get_history(stock_name, period = None, interval = "1d", auto_adjust = True, group_by = 'ticker', progress = False):
     dat = yf.Tickers(stock_name)
     result =  dat.history(
         period = period,
         interval = interval,
         auto_adjust = auto_adjust,
-        group_by = group_by
+        group_by = group_by,
+        progress = progress
         )
     if result is None or result.empty:#checking if the dataframe is empty
         return None
