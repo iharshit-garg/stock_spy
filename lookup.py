37d8ed321ba9a7e608634641a2e3cb601919fddf
Harshit Garg <harshitgarg@Harshits-MacBook-Air.local>
2026-03-19 22:00:10 -0400

    migrated from yfinance to massive's rest api

--- lookup.py
+++ lookup.py
@@ -1,36 +1,31 @@
-import yfinance as yf
+from client import client
 
-def lookup(security_name: str, instrument_type: str):
-    
-    instrument_types = ['cryptocurrency', 'etf', 'stock', 'index'] #supported instrument types
-    dat = yf.Lookup(security_name)
-    
-    if instrument_type in instrument_types:
-        looked_symbols = [] #initalizing list to add searched symbols
-        df = getattr(dat, instrument_type)
+def lookup(security_name: str):
+    tickers = [] #storing name, symbol for found symbols
 
-        if df is None or df.empty: #checking if the dataframe is empty
-             return None #returning none so that cli can decide what to show to the user
-        else:
-             i = 1
-             for idx, name in df['shortName'].head(10).items(): #getting shortName field from the dataframe, showing only top 10 items
-                print(i, idx, name)
-                looked_symbols.append(idx)
-                i += 1
+    for t in client.list_tickers(
+        search = security_name,
+        market = "stocks",
+        sort = "ticker",
+        limit = 10, #top 10 results
+    ):
+        tickers.append([t.name, t.ticker])
+    
+    if not tickers:
+        return None #returning None is no symbol is found for the searched security name so CLI can show the error gracefully
+    else:
+        for i, ticker in enumerate(tickers):
+            print(f"{i+1}. name: {ticker[0]}, symbol: {ticker[1]}")
         
-        get_symbol = input(f"Select a number (1-{len(looked_symbols)}): ")
+        get_symbol = input(f"Select a number (1-{len(tickers)}): ")
         try:
             get_symbol = int(get_symbol) #type -> int
-            if 1 <= get_symbol <= len(looked_symbols):
-                chosen_symbol = looked_symbols[get_symbol - 1]
-                print(f"You selected: {chosen_symbol}")
+            if 1 <= get_symbol <= len(tickers):
+                chosen_symbol = tickers[get_symbol - 1][1]
+                print(f"You chose: {chosen_symbol}")
                 return chosen_symbol #return symbol so that stockSpy can pass the symbol to get the info
             else:
-                 print("Out of range!")
-
+                print("Out of range!")
+            
         except ValueError:
-             print("Invalid!")
-
-    else:
-     print("\nInvalid type!\n")
-     return None
+            print("Invalid")
